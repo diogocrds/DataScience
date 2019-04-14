00a4c1ce188f78a9271c11506fc5531d5e2e8129
Diogo J. Cardoso <diogoopequeno@hotmail.com>
2019-04-14 18:27:22 -0300

    tutorial code 1

--- Titanic/TitanicDataAnalisys1.R
+++ Titanic/TitanicDataAnalisys1.R
@@ -30,3 +30,72 @@ ggplot(train, aes(x = Pclass, fill = factor(Survived))) +
   xlab("Pclass") +
   ylab("Total Count") +
   labs(fill = "Survived") 
+
+# Examine the first few names in the training data set
+head(as.character(train$Name))
+
+# How many unique names are there across both train & test?
+length(unique(as.character(data.combined$Name)))
+
+# Two duplicate names, take a closer look
+# First, get the duplicate names and store them as a vector
+dup.names <- as.character(data.combined[which(duplicated(as.character(data.combined$Name))), "Name"])
+
+# Next, take a look at the records in the combined data set
+data.combined[which(data.combined$Name %in% dup.names),]
+
+# What is up with the 'Miss.' and 'Mr.' thing?
+library(stringr)
+
+
+# Any correlation with other variables (e.g., sibsp)?
+misses <- data.combined[which(str_detect(data.combined$Name, "Miss.")),]
+misses[1:5,]
+
+
+# Hypothesis - Name titles correlate with age
+mrses <- data.combined[which(str_detect(data.combined$Name, "Mrs.")), ]
+mrses[1:5,]
+
+# Check out males to see if pattern continues
+males <- data.combined[which(data.combined$Sex == "male"), ]
+males[1:5,]
+
+# Expand upon the realtionship between `Survived` and `Pclass` by adding the new `Title` variable to the
+# data set and then explore a potential 3-dimensional relationship.
+
+# Create a utility function to help with title extraction
+# NOTE - Using the grep function here, but could have used the str_detect function as well.
+extractTitle <- function(name) {
+  name <- as.character(name)
+  
+  if (length(grep("Miss.", name)) > 0) {
+    return ("Miss.")
+  } else if (length(grep("Master.", name)) > 0) {
+    return ("Master.")
+  } else if (length(grep("Mrs.", name)) > 0) {
+    return ("Mrs.")
+  } else if (length(grep("Mr.", name)) > 0) {
+    return ("Mr.")
+  } else {
+    return ("Other")
+  }
+}
+
+# NOTE - The code below uses a for loop which is not a very R way of
+#        doing things
+titles <- NULL
+for (i in 1:nrow(data.combined)) {
+  titles <- c(titles, extractTitle(data.combined[i,"Name"]))
+}
+data.combined$Title <- as.factor(titles)
+
+# Since we only have survived lables for the train set, only use the
+# first 891 rows
+ggplot(data.combined[1:891,], aes(x = Title, fill = Survived)) +
+  geom_bar() +
+  facet_wrap(~Pclass) + 
+  ggtitle("Pclass") +
+  xlab("Title") +
+  ylab("Total Count") +
+  labs(fill = "Survived")
